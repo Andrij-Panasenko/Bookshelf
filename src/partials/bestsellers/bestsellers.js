791f76be762517563147671e709abfb539abfc02
Vlad <89925292+vladbutorin@users.noreply.github.com>
2023-10-13 20:11:14 +0300

    fhf

---
+++ src/partials/bestsellers/bestsellers.js
@@ -0,0 +1,12 @@
+// import './js/back-to-top';
+import './js/allBooksMarkup';
+import './js/bestsellers';
+import './js/fetchBooks';
+import './js/fetchSelectedBooks.js';
+import './js/oneBookMarkup';
+import './js/refs';
+import './js/render-main-title';
+import './js/renderSelectedCategory';
+import './js/seeMore';
+
+
