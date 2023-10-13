d7b1c2c57cc95850e4219624114b25f66aa8ea77
Vlad <89925292+vladbutorin@users.noreply.github.com>
2023-10-13 19:40:38 +0300

    grtdg

---
+++ src/partials/bestsellers/index.js
@@ -0,0 +1,12 @@
+// import './js/back-to-top';
+import '../bestsellers/js/allBooksMarkup';
+import '../bestsellers/js/bestsellers';
+import '../bestsellers/js/fetchBooks';
+import '../bestsellers/js/fetchSelectedBooks.js';
+import '../bestsellers/js/oneBookMarkup';
+import '../bestsellers/js/refs';
+import '../bestsellers/js/render-main-title';
+import '../bestsellers/js/renderSelectedCategory';
+import '../bestsellers/js/seeMore';
+
+
---
+++ src/partials/bestsellers/js/seeMore.js
@@ -0,0 +1,31 @@
+import getRefs from './refs';
+const { categoryContainerEl } = getRefs();
+
+import { fetchSelectedBooks } from './fetchSelectedBooks';
+import { renderBooksByCategory } from './renderSelectedCategory';
+import { scrollToTop } from './back-to-top';
+import { renderMainTitle } from './render-main-title';
+
+
+categoryContainerEl.addEventListener('click', seeMoreBtnClickHandler);
+
+async function seeMoreBtnClickHandler(e) {
+  if (!e.target.classList.contains('see-more-btn')) return;
+
+  const id = e.target.dataset.id;
+  const data = await fetchSelectedBooks(id);
+
+  renderMainTitle(id);
+
+  const categoriesBtn = document.querySelectorAll('.category-btn');
+  const activeCategoryBtn = document.querySelector('.active-category');
+  for (const btn of categoriesBtn) {
+    if (btn.dataset.id === id) {
+      btn.classList.add('active-category');
+    }
+  }
+  activeCategoryBtn.classList.remove('active-category');
+
+  renderBooksByCategory(data);
+  scrollToTop();
+}
