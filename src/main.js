6133f4681fc095b13c2e76f8680e5bad0ca3ecd0
AndriyMerzlikin <lastheroofme@gmail.com>
2023-10-14 22:28:54 +0300

    categories-підключив файли

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
-import { fetchTopBooks } from "./js/api/get-api-request";
-import { fetchBookByCategory } from "./js/api/category";
-import { fetchCatrgoryList } from "./js/api/category-list";
-import { fetchBookById } from "./js/api/get-book-by-id";
+import { fetchTopBooks } from './js/api/get-api-request';
+import { fetchBookByCategory } from './js/api/category';
+import { fetchCatrgoryList } from './js/api/category-list';
+import { fetchBookById } from './js/api/get-book-by-id';
 
 import './js/scroll-up';
 
-import {renderCardShoppingBook} from "./js/shoppinglist-markup";
-import { loadbook } from "./js/local-storage";
-import { checkingStorage } from "./js/local-storage";
-import { bookDelete } from "./js/shoppinglist-markup";
+import { renderCardShoppingBook } from './js/shoppinglist-markup';
+import { loadbook } from './js/local-storage';
+import { checkingStorage } from './js/local-storage';
+import { bookDelete } from './js/shoppinglist-markup';
 
-import { mobileMenu } from "./js/header.js/mobile-menu";
+import { mobileMenu } from './js/header.js/mobile-menu';
 mobileMenu();
 
-import "./js/support-ukraine";
+import './js/support-ukraine';
+import './js/all-categories/all-categories';
