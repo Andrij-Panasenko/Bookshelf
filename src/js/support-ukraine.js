f90618bdde384a4b3ced39ee18f22b77e00d056e
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2023-10-14 13:55:05 +0200

    imagesAndStylesAdd

--- src/js/support-ukraine.js
+++ src/js/support-ukraine.js
@@ -5,47 +5,47 @@ const organizations = [
     {
         title: 'Save the Children',
         url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-        img: 'images/Save_the_children.png',
+        img: 'images/save-the-children@1x.png',
     },
     {
         title: 'Project HOPE',
         url: 'https://www.projecthope.org/country/ukraine/',
-        img: 'images/hope.png',
+        img: 'images/project-hope@1x.png',
     },
     {
         title: 'UNITED24',
         url: 'https://u24.gov.ua/uk',
-        img: 'images/united24.png',
+        img: 'images/united24@1x.png',
     },
     {
         title: 'International Medical Corps',
         url: 'https://internationalmedicalcorps.org/country/ukraine/',
-        img: 'images/international.png',
+        img: 'images/intern-med-corp@1x.png',
     },
     {
         title: 'Medicins Sans Frontieres',
         url: 'https://www.msf.org/ukraine',
-        img: 'images/medecins.png',
+        img: 'images/med-sans@1x.png',
     },
     {
         title: 'RAZOM',
         url: 'https://www.razomforukraine.org/',
-        img: 'images/razom.png',
+        img: 'images/razom@1x.png',
     },
     {
         title: 'Action against hunger',
         url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-        img: 'images/razom.png',
+        img: 'images/intern-med-corp@1x.png',
     },
     {
         title: 'World vision',
         url: 'https://www.wvi.org/emergencies/ukraine',
-        img: 'images/word_vision.png',
+        img: 'images/world-vision@1x.png',
     },
     {
         title: 'Serhiy Prytula Charity Foundation',
         url: 'https://prytulafoundation.org/en',
-        img: 'images/sergiy_prytula.png',
+        img: 'images/sergiy-prytula@1x.png',
     },
 ];
 
@@ -108,10 +108,6 @@ function scrollUp() {
         updateButtonLabel();
         smoothScroll(organizationsList);
     }
-}
-
-function updateButtonLabel() {
-    orgBtn.textContent = currentPage < totalPages - 1 ? 'Вниз' : 'Вгору';
 }
 
 function smoothScroll(element) {
