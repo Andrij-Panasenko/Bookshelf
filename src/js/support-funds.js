a294e587da95d0de19a122b0580f8715971d3d60
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2023-10-15 18:47:18 +0200

    supportFundsJsAdd

---
+++ src/js/support-funds.js
@@ -0,0 +1,49 @@
+const supportUkraineFunds = [
+    {
+        title: 'Save the Children',
+        url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+        img: 'images/save-the-children@1x.png',
+    },
+    {
+        title: 'Project HOPE',
+        url: 'https://www.projecthope.org/country/ukraine/',
+        img: 'images/project-hope@1x.png',
+    },
+    {
+        title: 'UNITED24',
+        url: 'https://u24.gov.ua/uk',
+        img: 'images/united24@1x.png',
+    },
+    {
+        title: 'International Medical Corps',
+        url: 'https://internationalmedicalcorps.org/country/ukraine/',
+        img: 'images/intern-med-corp@1x.png',
+    },
+    {
+        title: 'Medicins Sans Frontieres',
+        url: 'https://www.msf.org/ukraine',
+        img: 'images/med-sans@1x.png',
+    },
+    {
+        title: 'RAZOM',
+        url: 'https://www.razomforukraine.org/',
+        img: 'images/razom@1x.png',
+    },
+    {
+        title: 'Action against hunger',
+        url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+        img: 'images/intern-med-corp@1x.png',
+    },
+    {
+        title: 'World vision',
+        url: 'https://www.wvi.org/emergencies/ukraine',
+        img: 'images/world-vision@1x.png',
+    },
+    {
+        title: 'Serhiy Prytula Charity Foundation',
+        url: 'https://prytulafoundation.org/en',
+        img: 'images/sergiy-prytula@1x.png',
+    },
+];
+
+export { supportUkraineFunds };
