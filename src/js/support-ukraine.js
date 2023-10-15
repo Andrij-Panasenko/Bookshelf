9a8161f64121bd5c2d863b1b76520dc09848143e
Andrij-Panasenko <andrii.panasenko295@gmail.com>
2023-10-15 21:17:58 +0300

    Revert "Merge pull request #33 from Andrij-Panasenko/support-Ukraine"
    This reverts commit 50163a3ce5c321e4a8f9377a448ee368529954fd, reversing
    changes made to abd96c4f31a292718ad26d938261b19bfd5ae900.

--- src/js/support-ukraine.js
+++ src/js/support-ukraine.js
@@ -1,43 +1,125 @@
-import $ from 'jquery';
-import 'slick-carousel';
-import { supportUkraineFunds } from './support-funds';
-import arrowIcon from '../images/icons.svg';
+const organizationsList = document.getElementById('organizations-list');
+const orgBtn = document.querySelector('.org-btn');
 
-const fundsContainer = document.querySelector('.organization-container');
-fundsContainer.insertAdjacentHTML('beforeend', generateSupportFundsMarkup(supportUkraineFunds));
-
-$('.organization-container').slick({
-  vertical: true,
-  dots: false,
-  infinite: true,
-  slidesToShow: 6,
-  slidesToScroll: 3,
-  prevArrow: '<div></div>',
-  nextArrow: `<div class="flex-btn"><button type="button" class="org-btn" aria-label="Next">
-          <svg width="22" height="22"><use href="${arrowIcon}#icon-arrow"></use></svg>
-        </button></div>`,
-
-  responsive: [
+const organizations = [
     {
-      breakpoint: 767,
-      settings: {
-        slidesToShow: 6,
-        slidesToScroll: 3,
-      },
+        title: 'Save the Children',
+        url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+        img: 'images/save-the-children@1x.png',
     },
-  ],
+    {
+        title: 'Project HOPE',
+        url: 'https://www.projecthope.org/country/ukraine/',
+        img: 'images/project-hope@1x.png',
+    },
+    {
+        title: 'UNITED24',
+        url: 'https://u24.gov.ua/uk',
+        img: 'images/united24@1x.png',
+    },
+    {
+        title: 'International Medical Corps',
+        url: 'https://internationalmedicalcorps.org/country/ukraine/',
+        img: 'images/intern-med-corp@1x.png',
+    },
+    {
+        title: 'Medicins Sans Frontieres',
+        url: 'https://www.msf.org/ukraine',
+        img: 'images/med-sans@1x.png',
+    },
+    {
+        title: 'RAZOM',
+        url: 'https://www.razomforukraine.org/',
+        img: 'images/razom@1x.png',
+    },
+    {
+        title: 'Action against hunger',
+        url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+        img: 'images/intern-med-corp@1x.png',
+    },
+    {
+        title: 'World vision',
+        url: 'https://www.wvi.org/emergencies/ukraine',
+        img: 'images/world-vision@1x.png',
+    },
+    {
+        title: 'Serhiy Prytula Charity Foundation',
+        url: 'https://prytulafoundation.org/en',
+        img: 'images/sergiy-prytula@1x.png',
+    },
+];
+
+const itemsPerPage = 6;
+let currentPage = 0;
+let isAnimating = false;
+
+
+const totalPages = Math.ceil(organizations.length / itemsPerPage);
+
+function createListItem(organization, index) {
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
+    const image = document.createElement('img');
+
+    const number = (index + 1).toString().padStart(2, '0');
+    const { img, url, title } = organization;
+
+    listItem.classList.add('list-item');
+
+    link.href = url;
+    link.target = '_blank';
+
+    link.appendChild(image);
+    image.src = img;
+    image.alt = title;
+
+    listItem.appendChild(document.createTextNode(number + ' '));
+    listItem.appendChild(link);
+
+    return listItem;
+}
+function updateList() {
+    const start = currentPage * itemsPerPage;
+    const end = start + itemsPerPage;
+
+    organizationsList.innerHTML = '';
+
+    const fragment = document.createDocumentFragment();
+    for (let i = start; i < end; i++) {
+        const adjustedIndex = i % organizations.length;
+        const listItem = createListItem(organizations[adjustedIndex], adjustedIndex);
+        fragment.appendChild(listItem);
+    }
+    organizationsList.appendChild(fragment);
+}
+
+function scrollUp() {
+    currentPage = (currentPage - 1 + organizations.length) % organizations.length;
+    updateList();
+}
+
+function scrollDown() {
+    currentPage = (currentPage + 1) % organizations.length;
+    updateList();
+}
+
+orgBtn.addEventListener('click', () => {
+    setTimeout(() => {
+        scrollUp();
+        }, 200);
 });
 
-function generateSupportFundsMarkup(supportFunds) {
-  const markup = supportFunds
-    .map((uaFund, index) => {
-      const listIndex = String(index + 1).padStart(2, '0');
-      return `<div><li class="support-column">
-  <span class="support-index">${listIndex}</span><a class="fund-link" href="${uaFund.url}" target="_blank" rel="noopener noreferrer">
-  <img class="list-item" src="${uaFund.img}" alt="${uaFund.title}"></a>
-</li></div>
-`;
-    })
-    .join('');
-  return markup;
-}
+organizationsList.addEventListener('wheel', (e) => {
+    setTimeout(() => {
+    if (e.deltaY > 0) {
+        scrollDown();
+    } else {
+        scrollUp();
+    }}, 200);
+});
+
+updateList();
+
+
+
+
