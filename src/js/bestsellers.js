f8139f3fb298e77884a852caa3645dfa0a617600
Vlad <89925292+vladbutorin@users.noreply.github.com>
2023-10-15 13:27:59 +0300

    beseller

---
+++ src/js/bestsellers.js
@@ -0,0 +1,11 @@
+import './js/allBooksMarkup';
+import './js/bestsellers';
+import './js/fetchBooks';
+import './js/fetchSelectedBooks.js';
+import './js/oneBookMarkup';
+import './js/refs';
+import './js/render-main-title';
+import './js/renderSelectedCategory';
+import './js/seeMore.js';
+
+
--- src/main.js
+++ src/main.js
@@ -3,3 +3,19 @@ import { fetchBookByCategory } from "./js/api/category";
 import { fetchCatrgoryList } from "./js/api/category-list";
 import { fetchBookById } from "./js/api/get-book-by-id";
 
+<<<<<<< Updated upstream
+=======
+import './js/scroll-up';
+
+import "./js/local-storage";
+import "./js/shoppinglist-markup";
+
+import { mobileMenu } from './js/header.js/mobile-menu';
+mobileMenu();
+
+import './js/support-ukraine';
+import './js/all-categories/all-categories';
+
+import './js/bestsellers';
+
+>>>>>>> Stashed changes
