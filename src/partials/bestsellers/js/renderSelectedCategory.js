58c7b16d07542db14c2fd66746cd73152ab71649
Andrij-Panasenko <andrii.panasenko295@gmail.com>
2023-10-15 13:55:08 +0300

    commit

--- src/partials/bestsellers/js/renderSelectedCategory.js
+++
@@ -1,9 +0,0 @@
-import getRefs from './refs';
-const { renderingContainer } = getRefs();
-
-import { makeCategoryBooksMarkup } from './allBooksMarkup';
-
-export function renderBooksByCategory(data) {
-  const markup = makeCategoryBooksMarkup(data);
-  renderingContainer.innerHTML = markup;
-}
