85cc2aa5a090253e36adf1126b31fb2bfd24c90c
alina513 <130853290+alina513@users.noreply.github.com>
2023-10-15 13:05:09 +0300

    switch

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { fetchTopBooks } from "./js/api/get-api-request";
 import { fetchBookByCategory } from "./js/api/category";
 import { fetchCatrgoryList } from "./js/api/category-list";
 import { fetchBookById } from "./js/api/get-book-by-id";
-import { mobileMenu } from "./js/header.js/mobile-menu";
-mobileMenu();
+import './js/header.js/mobile-menu.js';
+import './js/header.js/switch-color.js';
