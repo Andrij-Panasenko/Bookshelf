ab4683033e5744afe73e793234e14607904552b7
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-17 19:38:53 +0300

    delete comment from main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
-// import { fetchTopBooks } from './js/api/get-api-request';
 import { fetchBookByCategory } from './js/api/category';
 import { fetchCatrgoryList } from './js/api/category-list';
 import { fetchBookById } from './js/api/get-book-by-id';
 
 import './js/scroll-up';
-
-// import "./js/local-storage";
-// import "./js/shoppinglist-markup";
 
 import './js/header.js/mobile-menu.js';
 import './js/header.js/switch-color.js';
