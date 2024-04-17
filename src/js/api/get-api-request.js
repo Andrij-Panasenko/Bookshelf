199c3beba31cd7dab3a498400de8eb91635c14d0
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-17 19:42:19 +0300

    delete comments from get-api-request

--- src/js/api/get-api-request.js
+++ src/js/api/get-api-request.js
@@ -1,18 +1,13 @@
-import { warningNotify } from "../notify";
-import axios from "axios";
-import Notiflix from "notiflix";
+import { warningNotify } from '../notify';
+import axios from 'axios';
+import Notiflix from 'notiflix';
 
-//функція повертає масив об'єктів top-books. 
+//функція повертає масив об'єктів top-books.
 export function fetchTopBooks() {
-  // Notiflix.Loading.dots('Please wait');
   const BASE_URL = 'https://books-backend.p.goit.global/books/top-books';
 
   try {
-    // const response = await fetch(BASE_URL);
-    // const data = await response.json();
-
     const data = axios.get(BASE_URL);
-    // console.log(data);
     return data;
   } catch (error) {
     warningNotify();
