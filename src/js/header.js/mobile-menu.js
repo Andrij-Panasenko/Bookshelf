194b3e28a819ea147c84202d248bfe1cf14f9474
a.panasenko <andrii.panasenko295@gmail.com>
2024-05-10 21:04:52 +0300

    Update mobile-menu.js
    format

--- src/js/header.js/mobile-menu.js
+++ src/js/header.js/mobile-menu.js
@@ -1,22 +1,15 @@
-  function mobileMenu() {
-    const mobileMenu = document.querySelector(".mobile-menu");
-    const menuOpen = document.querySelector(".mobile-menu-open");
-const body = document.querySelector('body');
-    
-    menuOpen.addEventListener("click", changeMenu);
+function mobileMenu() {
+  const mobileMenu = document.querySelector('.mobile-menu');
+  const menuOpen = document.querySelector('.mobile-menu-open');
 
+  menuOpen.addEventListener('click', changeMenu);
 
-    function changeMenu () 
-    {const isMenuOpen =  menuOpen.getAttribute('aria-expanded') === "true" || false;
-menuOpen.setAttribute('aria-expanded', !isMenuOpen);
-mobileMenu.classList.toggle("is-open");
-menuOpen.classList.toggle("is-open");
-
-
+  function changeMenu() {
+    const isMenuOpen =
+      menuOpen.getAttribute('aria-expanded') === 'true' || false;
+    menuOpen.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+    menuOpen.classList.toggle('is-open');
+  }
 }
-};
 mobileMenu();
-
-
-
-
